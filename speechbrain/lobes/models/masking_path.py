5c36f5b09fe7f3fd504d7a583133cdf6280c46b7
rijalsankalp <tha075bct039@tcioe.edu.np>
2023-05-07 16:37:48 +0545

    Changes to the masking networkfor perceparator

---
+++ speechbrain/lobes/models/masking_path.py
@@ -0,0 +1,598 @@
+"""Library to support percepatron based masking.
+
+Authors
+ * Sankalpa Rijal
+ * Saroj Prasad Mainali
+"""
+
+import math
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import copy
+from speechbrain.nnet.linear import Linear
+from speechbrain.lobes.models.transformer.Transformer import TransformerEncoder
+from speechbrain.lobes.models.transformer.Transformer import PositionalEncoding
+from speechbrain.lobes.models.transformer.Conformer import ConformerEncoder
+import speechbrain.nnet.RNN as SBRNN
+from torch.nn.modules.activation import MultiheadAttention
+from speechbrain.nnet.activations import Swish
+
+
+EPS = 1e-8
+
+
+class GlobalLayerNorm(nn.Module):
+    """Calculate Global Layer Normalization.
+
+    Arguments
+    ---------
+       dim : (int or list or torch.Size)
+           Input shape from an expected input of size.
+       eps : float
+           A value added to the denominator for numerical stability.
+       elementwise_affine : bool
+          A boolean value that when set to True,
+          this module has learnable per-element affine parameters
+          initialized to ones (for weights) and zeros (for biases).
+
+    Example
+    -------
+    >>> x = torch.randn(5, 10, 20)
+    >>> GLN = GlobalLayerNorm(10, 3)
+    >>> x_norm = GLN(x)
+    """
+
+    def __init__(self, dim, shape, eps=1e-8, elementwise_affine=True):
+        super(GlobalLayerNorm, self).__init__()
+        self.dim = dim
+        self.eps = eps
+        self.elementwise_affine = elementwise_affine
+
+        if self.elementwise_affine:
+            if shape == 3:
+                self.weight = nn.Parameter(torch.ones(self.dim, 1))
+                self.bias = nn.Parameter(torch.zeros(self.dim, 1))
+            if shape == 4:
+                self.weight = nn.Parameter(torch.ones(self.dim, 1, 1))
+                self.bias = nn.Parameter(torch.zeros(self.dim, 1, 1))
+        else:
+            self.register_parameter("weight", None)
+            self.register_parameter("bias", None)
+
+    def forward(self, x):
+        """Returns the normalized tensor.
+
+        Arguments
+        ---------
+        x : torch.Tensor
+            Tensor of size [N, C, K, S] or [N, C, L].
+        """
+        # x = N x C x K x S or N x C x L
+        # N x 1 x 1
+        # cln: mean,var N x 1 x K x S
+        # gln: mean,var N x 1 x 1
+        if x.dim() == 3:
+            mean = torch.mean(x, (1, 2), keepdim=True)
+            var = torch.mean((x - mean) ** 2, (1, 2), keepdim=True)
+            if self.elementwise_affine:
+                x = (
+                    self.weight * (x - mean) / torch.sqrt(var + self.eps)
+                    + self.bias
+                )
+            else:
+                x = (x - mean) / torch.sqrt(var + self.eps)
+
+        if x.dim() == 4:
+            mean = torch.mean(x, (1, 2, 3), keepdim=True)
+            var = torch.mean((x - mean) ** 2, (1, 2, 3), keepdim=True)
+            if self.elementwise_affine:
+                x = (
+                    self.weight * (x - mean) / torch.sqrt(var + self.eps)
+                    + self.bias
+                )
+            else:
+                x = (x - mean) / torch.sqrt(var + self.eps)
+        return x
+
+
+class CumulativeLayerNorm(nn.LayerNorm):
+    """Calculate Cumulative Layer Normalization.
+
+       Arguments
+       ---------
+       dim : int
+        Dimension that you want to normalize.
+       elementwise_affine : True
+        Learnable per-element affine parameters.
+
+    Example
+    -------
+    >>> x = torch.randn(5, 10, 20)
+    >>> CLN = CumulativeLayerNorm(10)
+    >>> x_norm = CLN(x)
+    """
+
+    def __init__(self, dim, elementwise_affine=True, eps=1e-8):
+        super(CumulativeLayerNorm, self).__init__(
+            dim, elementwise_affine=elementwise_affine, eps=eps
+        )
+
+    def forward(self, x):
+        """Returns the normalized tensor.
+
+        Arguments
+        ---------
+        x : torch.Tensor
+            Tensor size [N, C, K, S] or [N, C, L]
+        """
+        # x: N x C x K x S or N x C x L
+        # N x K x S x C
+        if x.dim() == 4:
+            x = x.permute(0, 2, 3, 1).contiguous()
+            # N x K x S x C == only channel norm
+            x = super().forward(x)
+            # N x C x K x S
+            x = x.permute(0, 3, 1, 2).contiguous()
+        if x.dim() == 3:
+            x = torch.transpose(x, 1, 2)
+            # N x L x C == only channel norm
+            x = super().forward(x)
+            # N x C x L
+            x = torch.transpose(x, 1, 2)
+        return x
+
+
+def select_norm(norm, dim, shape, eps=1e-8):
+    """Just a wrapper to select the normalization type.
+    """
+
+    if norm == "gln":
+        return GlobalLayerNorm(dim, shape, elementwise_affine=True, eps=eps)
+    if norm == "cln":
+        return CumulativeLayerNorm(dim, elementwise_affine=True, eps=eps)
+    if norm == "ln":
+        return nn.GroupNorm(1, dim, eps=eps)
+    else:
+        return nn.BatchNorm1d(dim)
+
+
+class Encoder(nn.Module):
+    """Convolutional Encoder Layer.
+
+    Arguments
+    ---------
+    kernel_size : int
+        Length of filters.
+    in_channels : int
+        Number of  input channels.
+    out_channels : int
+        Number of output channels.
+
+    Example
+    -------
+    >>> x = torch.randn(2, 1000)
+    >>> encoder = Encoder(kernel_size=4, out_channels=64)
+    >>> h = encoder(x)
+    >>> h.shape
+    torch.Size([2, 64, 499])
+    """
+
+    def __init__(self, kernel_size=2, out_channels=64, in_channels=1):
+        super(Encoder, self).__init__()
+        self.conv1d = nn.Conv1d(
+            in_channels=in_channels,
+            out_channels=out_channels,
+            kernel_size=kernel_size,
+            stride=kernel_size // 2,
+            groups=1,
+            bias=False,
+        )
+        self.in_channels = in_channels
+
+    def forward(self, x):
+        """Return the encoded output.
+
+        Arguments
+        ---------
+        x : torch.Tensor
+            Input tensor with dimensionality [B, L].
+        Return
+        ------
+        x : torch.Tensor
+            Encoded tensor with dimensionality [B, N, T_out].
+
+        where B = Batchsize
+              L = Number of timepoints
+              N = Number of filters
+              T_out = Number of timepoints at the output of the encoder
+        """
+        # B x L -> B x 1 x L
+        if self.in_channels == 1:
+            x = torch.unsqueeze(x, dim=1)
+        # B x 1 x L -> B x N x T_out
+        x = self.conv1d(x)
+        x = F.relu(x)
+
+        return x
+
+
+class Decoder(nn.ConvTranspose1d):
+    """A decoder layer that consists of ConvTranspose1d.
+
+    Arguments
+    ---------
+    kernel_size : int
+        Length of filters.
+    in_channels : int
+        Number of  input channels.
+    out_channels : int
+        Number of output channels.
+
+
+    Example
+    ---------
+    >>> x = torch.randn(2, 100, 1000)
+    >>> decoder = Decoder(kernel_size=4, in_channels=100, out_channels=1)
+    >>> h = decoder(x)
+    >>> h.shape
+    torch.Size([2, 1003])
+    """
+
+    def __init__(self, *args, **kwargs):
+        super(Decoder, self).__init__(*args, **kwargs)
+
+    def forward(self, x):
+        """Return the decoded output.
+
+        Arguments
+        ---------
+        x : torch.Tensor
+            Input tensor with dimensionality [B, N, L].
+                where, B = Batchsize,
+                       N = number of filters
+                       L = time points
+        """
+
+        if x.dim() not in [2, 3]:
+            raise RuntimeError(
+                "{} accept 3/4D tensor as input".format(self.__name__)
+            )
+        x = super().forward(x if x.dim() == 3 else torch.unsqueeze(x, 1))
+
+        if torch.squeeze(x).dim() == 1:
+            x = torch.squeeze(x, dim=1)
+        else:
+            x = torch.squeeze(x)
+        return x
+
+
+class IdentityBlock:
+    """This block is used when we want to have identity transformation within the Dual_path block.
+
+    Example
+    -------
+    >>> x = torch.randn(10, 100)
+    >>> IB = IdentityBlock()
+    >>> xhat = IB(x)
+    """
+
+    def _init__(self, **kwargs):
+        pass
+
+    def __call__(self, x):
+        return x
+
+
+class PyTorchPositionalEncoding(nn.Module):
+    """Positional encoder for the pytorch transformer.
+
+    Arguments
+    ---------
+    d_model : int
+        Representation dimensionality.
+    dropout : float
+        Dropout drop prob.
+    max_len : int
+        Max sequence length.
+
+    Example
+    -------
+    >>> x = torch.randn(10, 100, 64)
+    >>> enc = PyTorchPositionalEncoding(64)
+    >>> x = enc(x)
+    """
+
+    def __init__(self, d_model, dropout=0.1, max_len=5000):
+        super(PyTorchPositionalEncoding, self).__init__()
+        self.dropout = nn.Dropout(p=dropout)
+
+        pe = torch.zeros(max_len, d_model)
+        position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
+        div_term = torch.exp(
+            torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model)
+        )
+        pe[:, 0::2] = torch.sin(position * div_term)
+        pe[:, 1::2] = torch.cos(position * div_term)
+        pe = pe.unsqueeze(0).transpose(0, 1)
+        self.register_buffer("pe", pe)
+
+    def forward(self, x):
+        """Returns the encoded output.
+
+        Arguments
+        ---------
+        x : torch.Tensor
+            Tensor shape [B, L, N],
+            where, B = Batchsize,
+                   N = number of filters
+                   L = time points
+        """
+        x = x + self.pe[: x.size(0), :]
+        return self.dropout(x)
+
+
+class Perceparator_Masking(nn.Module):
+    """The dual path model which is the basis for dualpathrnn, sepformer, dptnet.
+
+    Arguments
+    ---------
+    in_channels : int
+        Number of channels at the output of the encoder.
+    out_channels : int
+        Number of channels that would be inputted to the intra and inter blocks.
+    intra_model : torch.nn.module
+        Model to process within the chunks.
+    inter_model : torch.nn.module
+        model to process across the chunks,
+    num_layers : int
+        Number of layers of Dual Computation Block.
+    norm : str
+        Normalization type.
+    K : int
+        Chunk length.
+    num_spks : int
+        Number of sources (speakers).
+    skip_around_intra : bool
+        Skip connection around intra.
+    linear_layer_after_inter_intra : bool
+        Linear layer after inter and intra.
+    use_global_pos_enc : bool
+        Global positional encodings.
+    max_length : int
+        Maximum sequence length.
+
+    Example
+    ---------
+    >>> intra_block = SBTransformerBlock(1, 64, 8)
+    >>> inter_block = SBTransformerBlock(1, 64, 8)
+    >>> dual_path_model = Dual_Path_Model(64, 64, intra_block, inter_block, num_spks=2)
+    >>> x = torch.randn(10, 64, 2000)
+    >>> x = dual_path_model(x)
+    >>> x.shape
+    torch.Size([2, 10, 64, 2000])
+    """
+
+    def __init__(
+        self,
+        in_channels,
+        out_channels,
+        num_layers=1,
+        norm="ln",
+        K=200,
+        num_spks=2,
+        use_global_pos_enc=False,
+        max_length=20000,
+    ):
+        super(Perceparator_Masking, self).__init__()
+        self.K = K
+        self.num_spks = num_spks
+        self.num_layers = num_layers
+        self.norm = select_norm(norm, in_channels, 3)
+        self.conv1d = nn.Conv1d(in_channels, out_channels, 1, bias=False)
+        self.use_global_pos_enc = use_global_pos_enc
+
+        if self.use_global_pos_enc:
+            self.pos_enc = PositionalEncoding(max_length)
+
+        
+        self.latent = nn.parameter.Parameter(
+            torch.nn.init.trunc_normal_(
+                torch.zeros((128,1,256)), mean=0, std=0.02, a=-2, b=2
+            )
+        )
+
+        self.cross_attn_layer = MultiheadAttention(embed_dim=256, num_heads=8, dropout=0)
+        self.latent_trnfr_layer = MultiheadAttention(embed_dim=256, num_heads=8, dropout=0) 
+        self.LinearLatent1 = nn.Linear(256, 512)
+        self.LinearLatent2 = nn.Linear(512, 256)
+
+        self.conv2d = nn.Conv2d(
+            out_channels, out_channels * num_spks, kernel_size=1
+        )
+        self.end_conv1x1 = nn.Conv1d(out_channels, in_channels, 1, bias=False)
+        self.prelu = nn.PReLU()
+        self.activation = nn.ReLU()
+        # gated output layer
+        self.output = nn.Sequential(
+            nn.Conv1d(out_channels, out_channels, 1), nn.Tanh()
+        )
+        self.output_gate = nn.Sequential(
+            nn.Conv1d(out_channels, out_channels, 1), nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        """Returns the output tensor.
+
+        Arguments
+        ---------
+        x : torch.Tensor
+            Input tensor of dimension [B, N, L].
+
+        Returns
+        -------
+        out : torch.Tensor
+            Output tensor of dimension [spks, B, N, L]
+            where, spks = Number of speakers
+               B = Batchsize,
+               N = number of filters
+               L = the number of time points
+        """
+
+        # before each line we indicate the shape after executing the line
+
+        # [B, N, L]
+        x = self.norm(x)
+
+        # [B, N, L]
+        x = self.conv1d(x)
+        if self.use_global_pos_enc:
+            x = self.pos_enc(x.transpose(1, -1)).transpose(1, -1) + x * (
+                x.size(1) ** 0.5
+            )
+
+        # [B, N, K, S]
+        x, gap = self._Segmentation(x, self.K)
+        B, N, K, S = x.shape
+        x = x.permute(0, 3, 2, 1).reshape(B * S, K, N)
+        x=x.permute(1,0,2)
+        
+        #make latent array here
+        latent = self.latent.expand(-1,S,-1) # [B, N, 256]
+
+        for i in range(self.num_layers):
+          latent,_ = self.cross_attn_layer(x,latent,latent) # latent = pass x and latent
+          #optput latent size
+          latent,_ = self.latent_trnfr_layer(latent,latent,latent) # latent = pass latent to next layer
+          #pass latent to next layer
+
+
+        out = self.LinearLatent1(latent)
+        out = self.LinearLatent2(out)
+        x = self.prelu(out).permute(2,0,1).reshape(1,256,250,82)
+        # [B, N*spks, K, S]
+        x = self.conv2d(x)
+        B, _, K, S = x.shape
+
+        # [B*spks, N, K, S]
+        x = x.view(B * self.num_spks, -1, K, S)
+
+        # [B*spks, N, L]
+        x = self._over_add(x, gap)
+        x = self.output(x) * self.output_gate(x)
+
+        # [B*spks, N, L]
+        x = self.end_conv1x1(x)
+
+        # [B, spks, N, L]
+        _, N, L = x.shape
+        x = x.view(B, self.num_spks, N, L)
+        x = self.activation(x)
+
+        # [spks, B, N, L]
+        x = x.transpose(0, 1)
+
+        return x
+
+    def _padding(self, input, K):
+        """Padding the audio times.
+
+        Arguments
+        ---------
+        K : int
+            Chunks of length.
+        P : int
+            Hop size.
+        input : torch.Tensor
+            Tensor of size [B, N, L].
+            where, B = Batchsize,
+                   N = number of filters
+                   L = time points
+        """
+        B, N, L = input.shape
+        P = K // 2
+        gap = K - (P + L % K) % K
+        if gap > 0:
+            pad = (
+                torch.Tensor(torch.zeros(B, N, gap))
+                .type(input.dtype)
+                .to(input.device)
+            )
+            input = torch.cat([input, pad], dim=2)
+
+        _pad = (
+            torch.Tensor(torch.zeros(B, N, P))
+            .type(input.dtype)
+            .to(input.device)
+        )
+        input = torch.cat([_pad, input, _pad], dim=2)
+
+        return input, gap
+
+    def _Segmentation(self, input, K):
+        """The segmentation stage splits
+
+        Arguments
+        ---------
+        K : int
+            Length of the chunks.
+        input : torch.Tensor
+            Tensor with dim [B, N, L].
+
+        Return
+        -------
+        output : torch.tensor
+            Tensor with dim [B, N, K, S].
+            where, B = Batchsize,
+               N = number of filters
+               K = time points in each chunk
+               S = the number of chunks
+               L = the number of time points
+        """
+        B, N, L = input.shape
+        P = K // 2
+        input, gap = self._padding(input, K)
+        # [B, N, K, S]
+        input1 = input[:, :, :-P].contiguous().view(B, N, -1, K)
+        input2 = input[:, :, P:].contiguous().view(B, N, -1, K)
+        input = (
+            torch.cat([input1, input2], dim=3).view(B, N, -1, K).transpose(2, 3)
+        )
+
+        return input.contiguous(), gap
+
+    def _over_add(self, input, gap):
+        """Merge the sequence with the overlap-and-add method.
+
+        Arguments
+        ---------
+        input : torch.tensor
+            Tensor with dim [B, N, K, S].
+        gap : int
+            Padding length.
+
+        Return
+        -------
+        output : torch.tensor
+            Tensor with dim [B, N, L].
+            where, B = Batchsize,
+               N = number of filters
+               K = time points in each chunk
+               S = the number of chunks
+               L = the number of time points
+
+        """
+        B, N, K, S = input.shape
+        P = K // 2
+        # [B, N, S, K]
+        input = input.transpose(2, 3).contiguous().view(B, N, -1, K * 2)
+
+        input1 = input[:, :, :, :K].contiguous().view(B, N, -1)[:, :, P:]
+        input2 = input[:, :, :, K:].contiguous().view(B, N, -1)[:, :, :-P]
+        input = input1 + input2
+        # [B, N, L]
+        if gap > 0:
+            input = input[:, :, :-gap]
+
+        return input
+
